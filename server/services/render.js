95a00367743ba0f29dd7ec9e30b90f8c2d5ad79c
muskan-chauhan14 <chauhanmuskan1407@gmail.com>
2023-04-29 17:44:17 +0530

    User Management System

---
+++ server/services/render.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const { response } = require('express');
+
+
+exports.homeRoutes = (req,res) => {
+
+    //Make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+        .then(function(response){
+            // console.log(response.data);
+            res.render('index.ejs', {users:response.data});
+        })
+        .catch(err=>{
+            res.send(err);
+        }) 
+}
+
+exports.add_user = (req,res) => {
+    res.render('add_user.ejs');
+}
+
+exports.update_user = (req,res) => {
+    axios.get('http://localhost:3000/api/users', {params: {id:req.query.id}})
+        .then(function(userdata){
+            res.render("update_user",{user: userdata.data})
+        })
+        .catch(err=> {
+            res.send(err);
+        })
+}
